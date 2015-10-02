f64672f540d3e77a7287f1e8b280c21dbaec58d9
ch <ch@ch.cache-dns.local>
2015-10-02 17:13:29 +0800

    first commit

---
+++ data_csv.py
@@ -0,0 +1,9 @@
+#import csv
+#with open("pokemon.csv") as f:
+#   data = csv.reader(f)
+#   for line in data:
+#       print("id:{0},type:{1},name:{2}".format(line[0],line[1],line[2]))
+#import numpy as np
+#data = np.genfromtxt("pokemon.csv",skip_header=1,dtype=None,delimiter=',')
+#print(data)
+from xml.etree import ElementTree
